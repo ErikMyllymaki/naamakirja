4db5db516ee36de4135f3d07e83613444fd26191
ErikMyllymaki <erik.myl01@gmail.com>
2022-12-22 14:22:57 +0200

    committi

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import "./login.scss"
+
+export const Login = () => {
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos magni consectetur, iusto sit cumque delectus doloribus aut eveniet eligendi inventore, qui odio esse. Reprehenderit atque neque ipsam, quod labore maxime.</p>
+                    <span>Don't you have an account</span>
+                    <button>register</button>
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
