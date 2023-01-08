19f46ef9ff5745811fce68e9a24055c8121fc675
ErikMyllymaki <erik.myl01@gmail.com>
2023-01-08 18:44:01 +0200

    moi

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -2,6 +2,19 @@ import "./leftBar.scss"
 
 export default function LeftBar() {
   return (
-    <div>LeftBar</div>
-  )
+      <div className="leftBar">
+        <div className="container">
+          <div className="menu">
+            <div className="user">
+            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                <span>John Doe</span>
+            </div>
+            <div className="item">
+              <img src="" alt="" />
+              <span>Friends</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import "./navbar.scss"
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
@@ -10,14 +14,26 @@ export default function Navbar() {
             <div className="left">
                 <Link to="/" style={{textDecoration:"none"}}>
                     <span>Naamakirja</span>
+                </Link>
+
                     <HomeOutlinedIcon />
                     <DarkModeOutlinedIcon />
                     <GridViewOutlinedIcon />
-                </Link>
+                    <div className="search">
+                        <SearchIcon/>
+                        <input type="text" placeholder="Search..." />
+                    </div>
             </div>
             <div className="right">
-
+                <PersonOutlineOutlinedIcon />
+                <EmailOutlinedIcon />
+                <NotificationsNoneOutlinedIcon />
+                <div className="user">
+                <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                <span>John Doe</span>
             </div>
+            </div>
+
         </div>
     )
 }
