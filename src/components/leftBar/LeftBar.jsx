a2f31047ec2796519df6f5b8fc4b20c76ee2d9ac
ErikMyllymaki <erik.myl01@gmail.com>
2023-01-08 19:17:08 +0200

    weqewq

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,4 +1,6 @@
 import "./leftBar.scss"
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+
 
 export default function LeftBar() {
   return (
@@ -6,12 +8,24 @@ export default function LeftBar() {
         <div className="container">
           <div className="menu">
             <div className="user">
-            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            {/* <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" /> */}
                 <span>John Doe</span>
             </div>
             <div className="item">
-              <img src="" alt="" />
+            <PersonOutlineOutlinedIcon />
               <span>Friends</span>
+            </div>
+            <div className="item">
+            <PersonOutlineOutlinedIcon />
+              <span>Groups</span>
+            </div>
+            <div className="item">
+            <PersonOutlineOutlinedIcon />
+              <span>Marketplace</span>
+            </div>
+            <div className="item">
+            <PersonOutlineOutlinedIcon />
+              <span>Watch</span>
             </div>
           </div>
         </div>
